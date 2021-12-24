bbb192f1f1d36815c5abf5498b65329d731ee70b
Agancoder <noah.gao@outlook.com>
2021-12-24 09:11:03 +0800

    第三章

--- 第三章/练习3.36.cpp
+++ 第三章/练习3.36.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 
     if( ivec1 != ivec2 )
     {
-        cout << " 不相等" << endl;
+        cout << "不相等" << endl;
     }
     else
     {
--- 第三章/练习3.44.cpp
+++ 第三章/练习3.44.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main(int argc, char **argv)
-{
-    int array[10];
-
-    // -std=c++11
-    vector<int> ivec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    for (int i = 0; i < 10; i++)
-    {
-        array[i] = ivec[i];
-    }
-    return 0;
-}
+略
