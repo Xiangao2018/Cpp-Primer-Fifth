4b38ef4c16604dda6613848ce2e83ba08b5c7a19
Agancoder <noah.gao@outlook.com>
2021-12-27 22:31:24 +0800

    Source Code For chap 7

---
+++ 第07章/练习7.1.1.cpp
@@ -0,0 +1,61 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue      = 0.0f;
+
+    std::string isbn() const { return bookNo; }
+    void combine(const Sales_data &data) {
+        units_sold += data.units_sold;
+        revenue += data.revenue;
+    }
+};
+
+std::istream &read(std::istream &in, Sales_data &data)
+{
+    in >> data.bookNo >> data.units_sold >> data.revenue;
+
+    return cin;
+}
+
+std::ostream &print(std::ostream &out, const Sales_data &data)
+{
+    out << data.bookNo << " " << data.units_sold << " " << data.revenue;
+
+    return out;
+}
+
+
+Sales_data add(const Sales_data &data1, const Sales_data &data2)
+{
+    Sales_data sum = data1;
+    sum.units_sold += data2.units_sold;
+    sum.revenue += data2.revenue;
+
+    return sum;
+}
+
+int main(int argc, char **argv)
+{
+    Sales_data total;
+
+    if( read(cin, total) )
+    {
+        Sales_data trans;
+        while( read(cin, trans) ) {
+            if( total.isbn() == trans.isbn() ) {
+                total.combine(trans);
+            } else {
+                print(cout, total) << endl;
+                total = trans;
+            }               
+        }
+        print(cout, total);
+    } else {
+        cerr << "No data?!" << endl;
+    }
+}
