842f8e7c308dba47598c472a7b07525fb264842a
Agancoder <noah.gao@outlook.com>
2022-09-09 15:06:16 +0800

    Source Code

---
+++ 第09章/练习9.22.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	vector<int> iv{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+	// vector<int>::iterator iter = iv.begin(), mid = iv.begin + iv.size() / 2;
+
+	// while (iter != mid)
+	// {
+	// 	if( *iter == 5 )
+	// 		iv.insert(iter, 2 * 5);
+	// }
+
+
+
+	return 0;
+}
